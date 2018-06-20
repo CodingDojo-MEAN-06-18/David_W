40ca7032a5d89a54b83a218e475f5cf0cfbd893d
David Wukelic <wukebb24@hotmail.com>
2018-06-19 21:31:44 -0700

    hw editing

--- Week 1/DeckOfCards.js
+++ Week 1/DeckOfCards.js
@@ -1,67 +1,43 @@
 class DeckOfCards {
     constructor(){
         this.deck = []
-
     }
-    card(){
-        let card = "";
+    makeDeck() {
+        this.deck = []
+        const cards = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+        const suits = ["Diamonds","Hearts","Spades","Clubs"]
 
-        let num = Math.floor(Math.random() * 12); //generates number between 0-12
-        let establish_card = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
-        card = establish_card[num];
-
-        num = Math.floor(Math.random() * 4) //generates number between 0-3
-        let suit = ["Diamonds","Hearts","Spades","Clubs"]
-        card = card + " of " + suit[num];
-
-        return card;
-    }
-    makeDeck(){
-        var size = 52
-        for (var cards = 0; cards <= size; cards++){
-            let check_card = new DeckOfCards().card()
-            let card_inside = true
-            for (let index = 0; index <= this.deck.length; index++){
-                if (this.deck[index] == check_card) {
-                    card_inside = false;
-                }
-            }
-            if (card_inside == true){
-                this.deck.push(check_card) //if it doesnt find the card in the deck it inserts it
+        for (const suit of suits){
+            for(const card of cards){
+                this.deck.push(`${card} of ${suit}`);
             }
         }
-        // console.log(deck)
-        return this.deck
+
+        return this
     }
-// ill review later but here is the solution below that looks way better than mine haha
-    // reset() {
-    //     this.deck = [];
-    //     const suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades'];
-    //     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
-
-    //     for (const suit of suits) {
-    //         for (const value of values) {
-    //             this.deck.push(`${ value } of ${ suit }`);
-    //         }
-    //     }
-    //     return this;
-    // }
-
-    // shuffle() {
-    //     let m = this.deck.length, t, i;
-    //     while (m) {
-    //         i = Math.floor(Math.random() * m--);
-    //         t = this.deck[m];
-    //         this.deck[m] = this.deck[i];
-    //         this.deck[i] = t;
-    //     }
-    //     return this;
-    // }
-
-    deal() {
-        return this.deck.pop();
+    reset(){
+        var reset_Deck = new DeckOfCards();
+        return reset_Deck.makeDeck()
     }
-}
+
+    shuffle(){
+        var cards_left = this.deck.length, temp, index;
+
+        // While there remain elements to shuffle…
+        while (cards_left) {
+            // Pick a remaining element in the array
+            index = Math.floor(Math.random() * cards_left--);
+        
+            // And swap it with the current element that was just found
+            temp = this.deck[cards_left];
+            this.deck[cards_left] = this.deck[index];
+            this.deck[index] = temp;
+            // each iteration should push the shuffeled card to the back until they are all shuffled
+        }
+    
+        return this;
+        }
+    }
 
 class Player {
     constructor(name) {
@@ -81,12 +57,12 @@ class Player {
 }
 
 const deck1 = new DeckOfCards();
-console.log(deck1.makeDeck())
+console.log(deck1.makeDeck().shuffle())
 
-const player2 = new Player("Ronnel");
-player2.draw(deck1);
-console.log(player2);
-console.log(deck1);
+// const player2 = new Player("Ronnel");
+// player2.draw(deck1);
+// console.log(player2);
+// console.log(deck1);
 
 
 
