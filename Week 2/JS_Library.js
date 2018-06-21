a8862997fac1f668ae8a68d028ee5e476730bc8f
David Wukelic <wukebb24@hotmail.com>
2018-06-20 22:36:53 -0700

    find

--- Week 2/JS_Library.js
+++ Week 2/JS_Library.js
@@ -15,12 +15,27 @@ function each(arr, callback) {
           array[i] = callback(array[i]);
         }
       },
-
-    reduce: function() { 
-      // code here;
+      
+    reduce: function(array, [memo], callback)  { 
+        for(index = 0; index < array.length; index++){
+            if (!memo) {
+                memo = callback(array[index]);
+                index++;
+            }
+            array[index] = callback(array[index]);
+        }
     },
-    find: function() {   
-      // code here;
+    find: function(array, test, callback) {
+
+        // find_.find(list, predicate, [context]) Alias: detect 
+        // Looks through each value in the list, returning the first one that passes a 
+        // truth test (predicate), or undefined if no value passes the test. The function 
+        // returns as soon as it finds an acceptable element, and doesn't traverse the entire 
+        // list. predicate is transformed through iteratee to facilitate shorthand syntaxes.
+
+
+
+
     },
     filter: function() { 
       // code here;
