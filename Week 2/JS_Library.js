3455ae7e01282b3a77f1ff2404e6d89c3c509a2d
David Wukelic <wukebb24@hotmail.com>
2018-06-20 22:42:52 -0700

    reject

--- Week 2/JS_Library.js
+++ Week 2/JS_Library.js
@@ -48,11 +48,21 @@ function each(arr, callback) {
             return undefined;
         }
     },
-    reject: function() { 
-      // code here;
+    reject: function(array, predicate, callback) {
+        var not_passed = [];
+        for (index = 0; index < array.length; index++ ){
+            var new_predicate = callback(array[index]);
+            if (predicate != new_predicate){
+                not_passed.push(array[index]);
+            }
+        }
+        if (not_passed){
+            return not_passed;
+        } else {
+            return undefined;
+        }
     }
-  }
- // you just created a library with 5 methods!
+}
 
  var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
 console.log(evens); // if things are working right, this will return [2,4,6].
