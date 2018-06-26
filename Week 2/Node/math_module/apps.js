b8ac3e8cf58658392ddd01cefdd6647b29381416
David Wukelic <wukebb24@hotmail.com>
2018-06-26 16:34:33 -0700

    final touches

--- Week 2/Node/math_module/apps.js
+++ Week 2/Node/math_module/apps.js
@@ -3,39 +3,18 @@ var fs = require('fs');
 var my_module = require('./mathlib')();     //notice the extra invocation parentheses!
 // console.log(my_module);
 
+// math.add(2,3) should return 5)
 console.log(my_module.add(2,3));
 
-
-// math.add(2,3) should return 5)
-
 // multiply two numbers (e.g. math.multiply(3,5) should return 15)
+console.log(my_module.multiply(3,5));
 
 // square a number (e.g. math.square(5) should return 25)
+console.log(my_module.square(5));
 
 // return a random number between the two numbers 
 // (e.g. math.random(1,35) should return a random number between 1 and 35)
+console.log(my_module.random(1, 35));
 
-
-
-// var server = http.createServer(function (request, response){
-//     console.log('client request URL: ', request.url);
-
-//     if(request.url === '/') {
-//         fs.readFile('./views/index.html', 'utf8', function (errors, contents){
-//             response.writeHead(200, {'Content-Type': 'text/html'});
-//             response.write(contents); 
-//             response.end();
-//         });
-//     }
-//     else {
-//         fs.readFile('error.html', 'utf8', function (errors, contents){
-//             response.writeHead(200, {'Content-type': 'text/html'});
-//             response.write(contents); 
-//             response.end();
-//         });
-//    }
-// });
-// server.listen(8000);
-// console.log("Running in localhost at port 8000");
 
 
--- Week 2/Node/math_module/mathlib.js
+++ Week 2/Node/math_module/mathlib.js
@@ -1,29 +1,17 @@
 module.exports = function (){
     return {
-      add: function(num1, num2) { 
+        add: function(num1, num2) { 
            return (num1 + num2);
-      },
-      multiply: function(num1, num2) {
+        },
+        multiply: function(num1, num2) {
             return (num1 * num2);
-      },
-      square: function(num) {
-        return (num * num);
-
-      },
-      random: function(num1, num2) {
-           var x = Math.floor.random(num1, num2);
-           return x;
-      }
+        },
+        square: function(num) {
+            return (num * num);
+        },
+        random: function(num1, num2) {
+            var x = Math.floor((Math.random() * num2) + num1);
+            return x;
+        }
     }
-  };
-
-
-
-// math.add(2,3) should return 5)
-
-// multiply two numbers (e.g. math.multiply(3,5) should return 15)
-
-// square a number (e.g. math.square(5) should return 25)
-
-// return a random number between the two numbers 
-// (e.g. math.random(1,35) should return a random number between 1 and 35)
+};
