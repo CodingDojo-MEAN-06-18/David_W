e18433cfc1abe8fa30b2f105869e8331349aa35a
David Wukelic <wukebb24@hotmail.com>
2018-06-26 21:45:02 -0700

    express sample final touches

--- Week 3/Hello_Express/server.js
+++ Week 3/Hello_Express/server.js
@@ -19,6 +19,24 @@ app.use(express.static(__dirname + "/static"));
 // try printing out __dirname using console.log to see what it is and why we use it
 console.log(__dirname)
 
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+app.get("/users", function (request, response){
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
+
+
+
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 app.listen(8000, function() {
   console.log("listening on port 8000");
