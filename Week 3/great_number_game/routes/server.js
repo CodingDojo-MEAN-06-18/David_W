7d45663cec66d0f1b93082a3acfa331db5eb7290
David Wukelic <wukebb24@hotmail.com>
2018-07-01 21:13:53 -0700

    session working

--- Week 3/great_number_game/routes/server.js
+++ Week 3/great_number_game/routes/server.js
@@ -1,9 +1,17 @@
+var session = require('express-session');
 var count = 0;
 var express = require("express");
 // path module -- try to figure out where and why we use this
 var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
+
+app.use(session({
+    secret: 'keyboardkitteh',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  }))
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // static content
@@ -14,17 +22,29 @@ app.set('view engine', 'ejs');
 
 // root route to render the index.ejs view
 app.get('/', function(req, res) {
+    if (!req.session.number){
+        req.session.number = Math.floor((Math.random() * 100) + 1);
+    }    
+    // console.log(`Random number is ${req.session.number}`);
     res.render("index");
 })
 
 // route for results from the form
 app.post('/result', function(req, res) {
-    var results_req = req.body;
-    var data = {
-        input: req.body.input, 
+    // console.log(req.session.input);
+    if (req.session.number > req.body.input){
+        console.log("the number is higher");
+        res.redirect('/')
+
+    } else if (req.session.number < req.body.input){
+        console.log("the number is lower");
+        res.redirect('/')
+
+    } else {
+        console.log("you got it!!");
+        res.redirect('/')
     }
-    res.render("result", {data: data});
-   })
+})
 
 // tell the express app to listen on port 8000
 app.listen(8000, function() {
