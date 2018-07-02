6c48b17fe5fde90bff9aab34a210fb59af7a2464
David Wukelic <wukebb24@hotmail.com>
2018-07-01 20:04:13 -0700

    routes

---
+++ Week 3/great_number_game/routes/server.js
@@ -0,0 +1,32 @@
+var count = 0;
+var express = require("express");
+// path module -- try to figure out where and why we use this
+var path = require("path");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// static content
+// app.use(express.static(path.join(__dirname, "./static")));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+    res.render("index");
+})
+
+// route for results from the form
+app.post('/result', function(req, res) {
+    var results_req = req.body;
+    var data = {
+        input: req.body.input, 
+    }
+    res.render("result", {data: data});
+   })
+
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+});
