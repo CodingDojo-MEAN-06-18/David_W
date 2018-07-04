c9031df96f2e006c81154c580ccaa5518db2680d
David Wukelic <wukebb24@hotmail.com>
2018-07-04 13:19:08 -0700

    routes established

--- Week 3/survey_form_sockets/server.js
+++ Week 3/survey_form_sockets/server.js
@@ -1,1 +1,26 @@
-// server.js
+// all varibales required and needed to run express and our routes
+var path = require("path");
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var server = app.listen(8811, function() {
+    console.log("listening on port 8811");
+});
+var route = require('./routes/index.js')(app, server);
+
+
+// sets up bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static content
+app.use(express.static(path.join(__dirname, "./static")));
+
+// sets up views
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+    res.render("index");
+})
+
