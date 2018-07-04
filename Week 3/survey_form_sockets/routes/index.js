ee1e6630c504e5cb6a492abb16402984e6733aaa
David Wukelic <wukebb24@hotmail.com>
2018-07-04 13:31:08 -0700

    index.js comments

--- Week 3/survey_form_sockets/routes/index.js
+++ Week 3/survey_form_sockets/routes/index.js
@@ -1,12 +1,19 @@
+// exports the whole function to be used within other modules
 module.exports = function Route(app, server){
-	var io = require('socket.io').listen(server) 
+    // starts a new socket listening to the defined server
+    var io = require('socket.io').listen(server) 
+    // get information when the root route is visited and display the index.ejs file
 	app.get('/', function(req, res) {
 		res.render("index");
-	})
+    })
+    // listens to connection and then it starts the following function
 	io.sockets.on('connection', function (socket){
+        // the server will listen for the "posting_form" event then execute the following function
 	 	socket.on("posting_form", function (data){
-	 		var random_number = Math.floor((Math.random() * 1000) + 1);
-		  socket.emit('updated_message', {response: data}); 
+            //  created random integer from 1-1000
+            var random_number = Math.floor((Math.random() * 1000) + 1);
+            // emits  updated message and the random number it just generated to the client/frontend 
+		    socket.emit('updated_message', {response: data}); 
 			socket.emit('random_number', {response: random_number}); 
 		})
 	})
